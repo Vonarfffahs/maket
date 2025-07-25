992614efc5333fc4d440d4dfa9d6c94c2eb54f36
Victor <mhpd100.i@gmail.com>
2025-07-25 15:11:07 +0300

    slider.js fix

--- js/slider.js
+++ js/slider.js
@@ -1,20 +1,16 @@
 "use strict";
 
-const windowPathname = window.location.pathname;
-
 // Glide.js library for slider
-if (windowPathname == "/index.html" || windowPathname === "/") {
-  const config = {
-    type: "carousel",
-    perView: 3,
-    breakpoints: {
-      768: {
-        perView: 2,
-      },
-      480: {
-        perView: 1,
-      },
+const config = {
+  type: "carousel",
+  perView: 3,
+  breakpoints: {
+    768: {
+      perView: 2,
     },
-  };
-  new Glide(".glide", config).mount();
-}
+    480: {
+      perView: 1,
+    },
+  },
+};
+new Glide(".glide", config).mount();
